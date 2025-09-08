7984d0d2aae3f9fa8472b2acfd7ffc0287333410
Hugo <149089365+humarroquin@users.noreply.github.com>
2025-09-07 21:10:04 -0600

    added functionality to the no spaces checkbox

--- script.js
+++ script.js
@@ -5,24 +5,40 @@ const charCountDisplay = document.getElementById("char-count");
 const wordCountDisplay = document.getElementById("word-count");
 const sentenceCountDisplay = document.getElementById("sentence-count");
 const readingTimeDisplay = document.getElementById("reading-time");
+const excludeSpaces = document.getElementById("exclude-spaces");
 
-textArea.addEventListener("input", () => {
-  const charCount = textArea.value.split(" ").join("");
-  const wordCount = textArea.value
+function checkText() {
+  const text = textArea.value;
+  if (!text.trim()) return;
+
+  const charCount = text.split(" ").join("");
+  const charCountSpaces = text.length;
+  const wordCount = text
     .replace(/[!?\.]/g, "")
     .split(" ")
     .filter((word) => word !== "");
-  const sentenceCount = textArea.value
+  const sentenceCount = text
     .split(/[.!?]/)
     .filter((item) => item.trim() !== "");
+  const wordLength = wordCount.length;
 
-  charCountDisplay.textContent = charCount.length.toString().padStart(2, "0");
-  wordCountDisplay.textContent = wordCount.length.toString().padStart(2, "0");
-  sentenceCountDisplay.textContent = sentenceCount.length
-    .toString()
-    .padStart(2, "0");
+  function pad(num) {
+    return num.toString().padStart(2, "0");
+  }
+
+  if (excludeSpaces.checked) {
+    charCountDisplay.textContent = pad(charCount.length);
+  } else {
+    charCountDisplay.textContent = pad(charCountSpaces);
+  }
+
+  wordCountDisplay.textContent = pad(wordLength);
+  sentenceCountDisplay.textContent = pad(sentenceCount.length);
 
   const wpm = 200;
-  const readingTime = Math.ceil(wordCount.length / wpm);
+  const readingTime = Math.ceil(wordLength / wpm);
   readingTimeDisplay.textContent = readingTime < 1 ? "< 1" : readingTime;
-});
+}
+
+textArea.addEventListener("input", checkText);
+excludeSpaces.addEventListener("change", checkText);
